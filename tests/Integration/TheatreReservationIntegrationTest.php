9c5d98fc83337da460954bee47e7ecce47b3e9cb
Elvis OHIN <elvisohin33@gmail.com>
2024-05-28 16:04:03 +0200

    homepage

---
+++ tests/Integration/TheatreReservationIntegrationTest.php
@@ -0,0 +1,41 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use Slim\Factory\AppFactory;
+use Slim\Psr7\Factory\ServerRequestFactory;
+use Slim\Psr7\Factory\ResponseFactory;
+use Slim\Views\Twig;
+use Slim\Views\TwigMiddleware;
+use DI\Container;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\ORMSetup;
+
+class TheatreReservationIntegrationTest extends TestCase
+{
+    protected $app;
+
+    protected function setUp(): void
+    {
+        $container = new Container();
+        AppFactory::setContainer($container);
+        $this->app = AppFactory::create();
+
+        $container->set(Twig::class, function() {
+            return Twig::create(__DIR__ . '/../../templates', ['cache' => false]);
+        });
+
+        $this->app->add(TwigMiddleware::createFromContainer($this->app, Twig::class));
+
+        (require __DIR__ . '/../../config/routes.php')($this->app);
+    }
+
+    public function testHomePage()
+    {
+        $request = (new ServerRequestFactory)->createServerRequest('GET', '/');
+        $response = $this->app->handle($request);
+
+        $this->assertEquals(200, $response->getStatusCode());
+    }
+
+    
+}
